bc958a166f8039e4899615ea5e1fd79620c34e02
guguttemy <guguttemy@gmail.com>
2023-12-19 10:35:08 +0900

    Chapter1. 프로젝트 세팅 ~ Chapter2 스타일링 적용 방법까지

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// #1-1. Styling: 전역 CSS 적용
+import '@/app/ui/global.css'
+
+// DefaultLayout.jsx
+export default function RootLayout({children,}: { children: React.ReactNode; }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
 }
+
+/*
+   App.jsx
+
+   <DefaultLayout>
+    {children}
+   </DefaultLayout>
+
+*/
